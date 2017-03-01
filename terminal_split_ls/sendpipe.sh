ad8ce6904734e766651d074ff420196d0795a080
Tomas Meszaros <exo@tty.sk>
2017-03-01 17:08:31 +0100

    added terminal_split_ls

---
+++ terminal_split_ls/sendpipe.sh
@@ -0,0 +1,25 @@
+function cdd() {
+    PIPE=/tmp/catpipe
+    cd $@
+    echo "@clear" > $PIPE
+    echo "@pwd" > $PIPE
+    echo "@echo" > $PIPE
+    echo "@cd $@" > $PIPE
+    echo "@ls" > $PIPE
+}
+
+function sendpipe() {
+    PIPE=/tmp/catpipe
+    rm -r $PIPE
+    [[ ! -p $PIPE ]] && mkfifo $PIPE
+
+    while true; do
+        while read line; do
+            case "$line" in
+                @exit) rm -f $PIPE && exit 0;;
+                @*) eval "${line#@}" ;;
+                * ) echo "$line" ;;
+            esac
+        done <$PIPE
+    done
+}
